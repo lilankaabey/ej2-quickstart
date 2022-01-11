cc13789a4ae635f32e8ee1237fe522adec6d6e45
Lilanka Ravinath <inbox.lilanka@gmail.com>
2022-01-12 05:11:28 +0530

    add dialog

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,3 +3,16 @@ import { Button } from '@syncfusion/ej2-buttons';
 let button: Button = new Button();
 
 button.appendTo('#normalbtn');
+import { Dialog } from '@syncfusion/ej2-popups';
+
+let dialog = new Dialog({
+   content: 'This is a Dialog with Content',
+   target: document.getElementById("container"),
+   width: '250px'
+}); 
+
+dialog.appendTo('#dialog');
+
+document.getElementById("targetButton").onclick = (): void => {
+   dialog.show()
+}
