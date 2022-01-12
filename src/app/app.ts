2a625745bcb5dca50b80c94dc430b22882744a43
Lilanka Ravinath <inbox.lilanka@gmail.com>
2022-01-12 11:38:33 +0530

    add treeview and bind data

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,62 @@
-import { Button } from '@syncfusion/ej2-buttons';
+import { TreeView } from '@syncfusion/ej2-navigations';
+import { enableRipple } from '@syncfusion/ej2-base';
 
-let button: Button = new Button();
+enableRipple(true);
 
-button.appendTo('#normalbtn');
+// define array of JSON
+let data: { [key: string]: Object } [] = [
+   {
+      nodeId: '01',
+      nodeText: 'Music',
+      nodeChild: [
+         {
+            nodeId: '01-01',
+            nodeText: 'Gouttes.mp3'
+         }
+      ]
+   },
+   {
+      nodeId: '02',
+      nodeText: 'Videos',
+      expanded: true,
+      nodeChild: [
+         {
+            nodeId: '02-01',
+            nodeText: 'Natular.mp4'
+         },
+         {
+            nodeId: '02-02',
+            nodeText: 'Wild.mpeg'
+         }
+      ]
+   },
+   {
+      nodeId: '03',
+      nodeText: 'Documents',
+      nodeChild: [
+         {
+            nideId: '03-01',
+            nodeText: 'something.docx'
+         },
+         {
+            nodeId: '03-02',
+            nodeText: 'something2.ppt'
+         },
+         {
+            nodeId: '03-03',
+            nodeText: 'something3.pdf'
+         }
+      ]
+   }
+];
+
+let treeViewInstance = new TreeView({
+   fields: { 
+      dataSource: data,
+      id: 'nodeId',
+      text: 'nodeText',
+      child: 'nodeChild'
+   }
+});
+
+treeViewInstance.appendTo('#tree');
