a42ed1d169511d6235d6c20a1eefa34d9bd7349c
Lilanka Ravinath <inbox.lilanka@gmail.com>
2022-01-12 07:49:37 +0530

    add position

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,49 +1,83 @@
 import { Dialog } from '@syncfusion/ej2-popups';
-import { enableRipple } from '@syncfusion/ej2-base';
-
-enableRipple(true);
+import { RadioButton } from '@syncfusion/ej2-buttons';
 
 //Initialize Dialog Component
 let dialog = new Dialog({
 
    /* 
-      The Dialog supports to drag within its target container by grabbing the Dialog header, 
-      which allows the user to reposition the Dialog dynamically.
+      The Dialog can be positioned using the position property by providing the X and Y co-ordinates.
+      It can be positioned inside the target of the container or <body> of the element 
+      based on the given X and Y values.
+
+      for X is: left, center, right (or) any offset value 
+      for Y is: top, center, bottom (or) any offset value
    */
-   // enable the footer buttons
-   buttons: [
-      {
-         // click the footer button to hide the dialog
-         'click': () => { dialog.hide();},
-         // accessing button component properties by buttonModel property
-         buttonModel: {
-            // enable the primary button
-            isPrimary: true,
-            content: 'Ok'
-         }
-      },
-      {
-         'click': () => { dialog.hide();},
-         buttonModel: {
-            content: 'Cancel',
-            cssClass: 'e-flat'
-         }
-      }
-   ],
+   
+   // set Dialog positon
+   position: { X: 'center', Y: 'center'},
+   // disable the Esc key option to hide the dialog
+   closeOnEscape: false,
    // enable the draggable option
    allowDragging: true,
    //enable the header
-   header: 'Dialog',
+   header: 'Choose a Dialog Position',
    // enable the close icon button in header
    showCloseIcon: true,
-   // dialog content
-   content: 'This is a Dialog with Primary Button and Buttton',
-   target: document.getElementById("container"),
-   width: '250px'
+   // the dialog shows within the target element
+   target: document.getElementById("target"),
+   // dialog width
+   width: '450px',
+   open: dialogOpen,
+   close: dialogClose,
+   // dialog footer template
+   footerTemplate: '<span id="posvalue" style="float:left;margin-left:8px;padding:10px;">Position: { X: "Center", Y: "Center" }</span>'
 }); 
 
+// render intialized Dialog
 dialog.appendTo('#dialog');
 
-document.getElementById("targetButton").onclick = (): void => {
+let onChangeHandler: any = (args: any) => {
+   dialog.position.X = args.value.split(' ')[0];
+   dialog.position.Y = args.value.split(' ')[1];
+   let txt: string[] = args.event.target.parentElement.querySelector('.e-label').innerText.split(' ');
+   document.getElementById('posvalue').innerHTML = 'Position: { X: "' + txt[0] + '", Y: "' + txt[1] + '" }';
+};
+
+let radioButton: RadioButton = new RadioButton({label: 'Left Top', value: 'left top', change: onChangeHandler });
+radioButton.appendTo('#radio1');
+
+radioButton = new RadioButton({label: 'Center Top', value: 'center top', change: onChangeHandler});
+radioButton.appendTo('#radio2');
+
+radioButton = new RadioButton({label: 'Right Top', value: 'right top', change: onChangeHandler});
+radioButton.appendTo('#radio3');
+
+radioButton = new RadioButton({label: 'Left Center', value: 'left center', change: onChangeHandler});
+radioButton.appendTo('#radio4');
+
+radioButton = new RadioButton({label: 'Center Center', value: 'center center', change: onChangeHandler, checked: true});
+radioButton.appendTo('#radio5');
+
+radioButton = new RadioButton({label: 'Right Center', value: 'right center', change: onChangeHandler});
+radioButton.appendTo('#radio6');
+
+radioButton = new RadioButton({label: 'Left Bottom', value: 'left bottom', change: onChangeHandler});
+radioButton.appendTo('#radio7');
+
+radioButton = new RadioButton({label: 'Center Bottom', value: 'center bottom', change: onChangeHandler});
+radioButton.appendTo('#radio8');
+
+radioButton = new RadioButton({label: 'Right Bottom', value: 'right bottom', change: onChangeHandler});
+radioButton.appendTo('#radio9');
+
+document.getElementById("dialogBtn").onclick = (): void => {
    dialog.show();
+}
+
+function dialogOpen(): void {
+   document.getElementById('dialogBtn').style.display ='none';
+}
+
+function dialogClose(): void {
+   document.getElementById('dialogBtn').style.display ='block';
 }
